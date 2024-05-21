a54c98adab07b4b1be1240a28e5f940dfc7c34f9
Robson Rocha <135916984+robsonrccha@users.noreply.github.com>
2024-05-21 14:10:39 -0300

    First Commit

---
+++ EstruturasRepeticao/src/robsonrccha/com/github/LacoDo.java
@@ -0,0 +1,41 @@
+package robsonrccha.com.github;
+
+import java.util.Scanner;
+
+public class LacoDo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// Exemplo de contar carneirinhos
+		for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+		
+		
+		
+		/* 
+		  FOR POSITION:
+		
+		  int carneirinhos = 1; -> O programa entende que a variável carneirinhos,
+		  começa com o valor igual a 1 e isso acontece somente uma vez;
+		  
+		  carneirinhos < = 20; -> O programa verifica se a variável carneirinhos, ainda
+		  é menor que 20;
+		  
+		  O programa começa a executar o algoritmo, no nosso caso, imprimir a
+		  quantidade de carneirinhos em contagem;
+		  
+		  carneirinhos ++ -> O programa aumenta em mais 1, o valor da variável
+		  carneirinhos;
+		  
+		  O fluxo é finalizado, quando a variável carneirinhos for igual a 20.
+		 */
+		
+		
+
+		sc.close();
+	}
+
+}
