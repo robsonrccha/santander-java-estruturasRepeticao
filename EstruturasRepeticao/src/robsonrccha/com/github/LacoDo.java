60481b63a409477bf707f538845be1169db9f923
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-22 20:07:49 -0300

    Update Laço FOR

--- EstruturasRepeticao/src/robsonrccha/com/github/LacoDo.java
+++ EstruturasRepeticao/src/robsonrccha/com/github/LacoDo.java
@@ -10,31 +10,42 @@ public class LacoDo {
 
 		// Exemplo de contar Carneirinhos
 		for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
-			System.out.println(carneirinhos + " - Carneirinho(s)");
+			System.out.println("\n" + carneirinhos + " - Carneirinho(s)");
 		}
-		
-		
-		/* 
-		  FOR POSITION:
-		
-		  int carneirinhos = 1; -> O programa entende que a variável carneirinhos,
-		  começa com o valor igual a 1 e isso acontece somente uma vez;
-		  
-		  carneirinhos < = 20; -> O programa verifica se a variável carneirinhos, ainda
-		  é menor que 20;
-		  
-		  O programa começa a executar o algoritmo, no nosso caso, imprimir a
-		  quantidade de carneirinhos em contagem;
-		  
-		  carneirinhos ++ -> O programa aumenta em mais 1, o valor da variável
-		  carneirinhos;
-		  
-		  O fluxo é finalizado, quando a variável carneirinhos for igual a 20.
+
+		/*
+		 * FOR POSITION:
+		 * 
+		 * int carneirinhos = 1; -> O programa entende que a variável carneirinhos,
+		 * começa com o valor igual a 1 e isso acontece somente uma vez;
+		 * 
+		 * carneirinhos < = 20; -> O programa verifica se a variável carneirinhos, ainda
+		 * é menor que 20;
+		 * 
+		 * O programa começa a executar o algoritmo, no nosso caso, imprimir a
+		 * quantidade de carneirinhos em contagem;
+		 * 
+		 * carneirinhos ++ -> O programa aumenta em mais 1, o valor da variável
+		 * carneirinhos;
+		 * 
+		 * O fluxo é finalizado, quando a variável carneirinhos for igual a 20.
 		 */
 		
 		
-
+		//estrutura 2
+		//o que importa é somente o bloco condicional
+		System.out.println("\nDiga ate quantos carneirinhos para pegar no sono?");
+		int carneirinhos = sc.nextInt();
+		int qtdCarneirinhos = 1;
+		
+		for (; qtdCarneirinhos <= carneirinhos;) { // a variavelpode ser colocada fora do laço FOR
+            System.out.println("\n" + qtdCarneirinhos + " - Carneirinho(s)");
+            qtdCarneirinhos++; // o incremento deve estar dentro do bloco de codigo
+		}
+		
+		System.out.println("\nBons sonhos zzZ");
+		
 		sc.close();
-	}
-
+		
+		}
 }
