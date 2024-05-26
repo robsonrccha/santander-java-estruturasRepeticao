24d8320f4bbfadfc08c23880e1c4047fa13b6d79
Robson Rocha <robsonrccha@gmail.com>
2024-05-26 15:00:25 -0300

    Update ForArrays and While

--- EstruturasRepeticao/src/robsonrccha/com/github/While.java
+++ EstruturasRepeticao/src/robsonrccha/com/github/While.java
@@ -4,29 +4,29 @@ import java.text.DecimalFormat;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class While {
-    
-	//METODO PARA DETERMINAR VALORES DOS DOCES
-    private static double valorAleatorio() {
-        return ThreadLocalRandom.current().nextDouble(2, 15);
-    }
 
-    public static void main(String[] args) {
-        
-        DecimalFormat df = new DecimalFormat("#.##"); //FORMATACAO DO VALOR DOUBLE EM DUAS CASAS DECIMAIS
-        double mesada = 50;
+	// METODO PARA DETERMINAR VALORES DOS DOCES
+	private static double valorAleatorio() {
+		return ThreadLocalRandom.current().nextDouble(2, 15);
+	}
 
-        while(mesada > 0) {
-            double valorDoce = valorAleatorio(); //ATRIBUIÇÃO DO METODO AO VALOR DO DOCE
-            if(valorDoce > mesada)
-                valorDoce = mesada;
-            System.out.println("Valor da mesada atual R$ " + df.format(mesada));
+	public static void main(String[] args) {
 
-            System.out.println("\nDoce de valor R$ " + df.format(valorDoce) + " adicionado no carrinho"); // Formatar o valor do doce
-            mesada -= valorDoce;
-        }
-        System.out.println("\nMesada: " + df.format(mesada)); // Formatar o valor da mesada
-        System.out.println("\nJoãozinho gastou toda a sua mesada em doces");
+		DecimalFormat df = new DecimalFormat("#.##"); // FORMATACAO DO VALOR DOUBLE EM DUAS CASAS DECIMAIS
+		double mesada = 50;
 
-    }
+		while (mesada > 0) {
+			double valorDoce = valorAleatorio(); // ATRIBUIÇÃO DO METODO AO VALOR DO DOCE
+			if (valorDoce > mesada)
+				valorDoce = mesada;
+			System.out.println("Valor da mesada atual R$ " + df.format(mesada));
+
+			System.out.println("\nDoce de valor R$ " + df.format(valorDoce) + " adicionado no carrinho");
+			mesada -= valorDoce;
+		}
+		System.out.println("\nMesada: " + df.format(mesada));
+		System.out.println("\nJoãozinho gastou toda a sua mesada em doces");
+
+	}
 
 }
